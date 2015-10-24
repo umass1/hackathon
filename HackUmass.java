cc9cb68551ec9a849d7fda55c6dd5e174689892e
Matthew Gramigna <mgramigna@umass.edu>
2015-10-24 00:26:10 -0400

    Hand sensor working

---
+++ HackUmass.java
@@ -0,0 +1,60 @@
+import java.io.IOException;
+import java.lang.Math;
+import com.leapmotion.leap.*;
+import com.leapmotion.leap.Gesture.State;
+
+class SampleListener extends Listener {
+    public void onInit(Controller controller) {
+        System.out.println("Initialized");
+    }
+
+    public void onConnect(Controller controller) {
+        System.out.println("Connected");
+        controller.enableGesture(Gesture.Type.TYPE_SWIPE);
+        controller.enableGesture(Gesture.Type.TYPE_CIRCLE);
+        controller.enableGesture(Gesture.Type.TYPE_SCREEN_TAP);
+        controller.enableGesture(Gesture.Type.TYPE_KEY_TAP);
+    }
+
+    public void onDisconnect(Controller controller) {
+        //Note: not dispatched when running in a debugger.
+        System.out.println("Disconnected");
+    }
+
+    public void onExit(Controller controller) {
+        System.out.println("Exited");
+    }
+
+    public void onFrame(Controller controller) {
+        // Get the most recent frame and report some basic information
+        Frame frame = controller.frame();
+        System.out.println("Frame id: " + frame.id()
+                         + ", timestamp: " + frame.timestamp()
+                         + ", hands: " + frame.hands().count()
+                         + ", fingers: " + frame.fingers().count()
+                         + ", tools: " + frame.tools().count()
+                         + ", gestures " + frame.gestures().count());
+    }
+}
+
+class HackUmass {
+    public static void main(String[] args) {
+        // Create a sample listener and controller
+        SampleListener listener = new SampleListener();
+        Controller controller = new Controller();
+
+        // Have the sample listener receive events from the controller
+        controller.addListener(listener);
+
+        // Keep this process running until Enter is pressed
+        System.out.println("Press Enter to quit...");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Remove the sample listener when done
+        controller.removeListener(listener);
+    }
+}
